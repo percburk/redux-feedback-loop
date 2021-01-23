c927b9def645e31edae81407caff78e6eb93901c
Kevin Burk <percburk@gmail.com>
2021-01-23 15:21:22 -0600

    dialog working for reset, comments added on reducer

--- src/components/ViewResults/ViewResults.jsx
+++ src/components/ViewResults/ViewResults.jsx
@@ -1,11 +1,23 @@
+import {
+  Box,
+  Typography,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { Box, Typography, Button } from '@material-ui/core';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import axios from 'axios';
 
 function ViewResults() {
   const history = useHistory();
+  const dispatch = useDispatch();
   const results = useSelector((state) => state.feedbackReducer);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   const handleSubmit = () => {
     axios
@@ -14,10 +26,27 @@ function ViewResults() {
       .catch((err) => alert('error in POST', err));
   };
 
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleDialogClose = (clicked) => {
+    if (clicked === 'no') {
+      setDialogOpen(false);
+    } else if (clicked === 'yes') {
+      dispatch({
+        type: 'CLEAR_KEEP_NAME',
+        payload: results.name,
+      })
+      history.push('/feeling');
+    }
+  };
+
   return (
     <Box textAlign="center">
       <Typography>You're in view results!</Typography>
       <Box>
+        <Typography>My name: {results.name}</Typography>
         <Typography>How I'm feeling: {results.feeling}</Typography>
         <Typography>How I'm understanding: {results.understanding}</Typography>
         <Typography>How supported: {results.support}</Typography>
@@ -26,6 +55,33 @@ function ViewResults() {
       <Button variant="contained" color="primary" onClick={handleSubmit}>
         Submit
       </Button>
+      <Button variant="contained" color="secondary" onClick={handleDialogOpen}>
+        Reset
+      </Button>
+      <Dialog open={dialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>{'Are you sure you want to reset?'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+          Your will need to start your feedback from the beginning.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => handleDialogClose('no')}
+          >
+            No
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => handleDialogClose('yes')}
+          >
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
--- src/components/redux/reducers/feedbackReducer.js
+++ src/components/redux/reducers/feedbackReducer.js
@@ -1,22 +1,35 @@
-/*
-  stores the feedback as an object, each piece of the feedback adding
-  a new key: value pair
-*/
-
 const feedbackReducer = (state = {}, action) => {
+  // adds to feedback object, eventually submitted to db to view on admin
   switch (action.type) {
+    // 'ADD_NAME' on Home, required
     case 'ADD_NAME':
       return { ...state, name: action.payload };
+
+    // 'ADD_FEELING' on Feeling, required
     case 'ADD_FEELING':
       return { ...state, feeling: action.payload };
+
+    // 'ADD_UNDERSTANDING' on Understanding, required
     case 'ADD_UNDERSTANDING':
       return { ...state, understanding: action.payload };
+
+    // ADD_SUPPORT' on Support, required
     case 'ADD_SUPPORT':
       return { ...state, support: action.payload };
+
+    // 'ADD_COMMENTS' on Comments, not required
+
     case 'ADD_COMMENTS':
       return { ...state, comments: action.payload };
+
+    // 'CLEAR' is called when feedback is submitted to db
     case 'CLEAR':
       return {};
+
+    // 'CLEAR_KEEP_NAME' is called on ViewResults to reset feedback, keep name
+    case 'CLEAR_KEEP_NAME':
+      return { name: action.payload };
+
     default:
       return state;
   }
